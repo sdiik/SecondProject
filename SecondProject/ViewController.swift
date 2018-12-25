07da1e75532028b4d85627e97468dc6aab648fde
sdik <ahmadshiddiq0@gmail.com>
2018-12-25 10:28:18 +0700

    Initial Commit

---
+++ SecondProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondProject
+//
+//  Created by ahmad shiddiq on 25/12/18.
+//  Copyright © 2018 ahmad shiddiq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
